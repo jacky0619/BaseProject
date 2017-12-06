af6017c5c1d4d49788aea63a5e53f8e21b6a383a
Jacky <jacky5454231@gmail.com>
2017-12-06 12:05:03 +0800

    update

---
+++ src/test/java/com/github/jacky0619/DemoTest.java
@@ -0,0 +1,17 @@
+package com.github.jacky0619;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DemoTest {
+    @Test
+    public void getType() throws Exception {
+        Triangle triangle=new Triangle(5,10,5)
+
+            String resault=triangle.getType();
+
+        Assert.assertEquals("是不是三角形", result);
+    }
+
+}
